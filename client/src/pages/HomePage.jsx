b351c2a1954d8709cdd284550d2fe25b98c275a1
Khaledabdelnasserr <khaledmaster999@gmail.com>
2025-05-23 18:57:02 +0300

    fix error

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,13 +4,8 @@ import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 import { ChatContext } from '../../context/ChatContext'
-
-
 const HomePage = () => {
-
    const { selectedUser } = useContext(ChatContext);
-
-
    return (
       <div className='layout-screen'>
          <div className={`home-page-style ${selectedUser ? 'grid-1' : 'grid-2'}`}>
